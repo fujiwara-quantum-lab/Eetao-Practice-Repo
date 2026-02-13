52c29eeea881d2ff7a3a0a62d9ab1210d52af801
Cora <63414958+cjfujiwara@users.noreply.github.com>
2026-02-13 15:39:48 -0500

    upload images

---
+++ foo.m
@@ -0,0 +1,7 @@
+function [outputArg1,outputArg2] = foo(inputArg1,inputArg2)
+%FOO Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
